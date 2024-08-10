ef59b6fb02fa93af55a07f19cf8c03bc0e21dd21
Luiz R. <luizroberto44630@gmail.com>
2024-08-10 15:08:12 -0300

    commit

--- includes/templates/concursos/details.php
+++ includes/templates/concursos/details.php
@@ -20,7 +20,6 @@
             </div>
         </div>
         <div class="ac-grid_numbers">
-<<<<<<< HEAD
             <button class="ac-grid_item chosen">0001</button>
             <button class="ac-grid_item chosen">0002</button>
             <button class="ac-grid_item">0003</button>
@@ -39,26 +38,6 @@
         </div>
         <div class="ac-show_numbers">
             <h4 class="h4-title ">Números Selecionados</h4>
-=======
-            <div class="ac-grid_item chosen">01</div>
-            <div class="ac-grid_item chosen">02</div>
-            <div class="ac-grid_item">03</div>
-            <div class="ac-grid_item">04</div>
-            <div class="ac-grid_item selected">05</div>
-            <div class="ac-grid_item selected">06</div>
-            <div class="ac-grid_item selected">07</div>
-            <div class="ac-grid_item">08</div>
-            <div class="ac-grid_item">09</div>
-            <div class="ac-grid_item chosen">10</div>
-            <div class="ac-grid_item">18</div>
-            <div class="ac-grid_item">29</div>
-            <div class="ac-grid_item chosen">10</div>
-            <div class="ac-grid_item">78</div>
-            <div class="ac-grid_item">99</div>
-        </div>
-        <div class="ac-show_numbers">
-            <h4 class="h4-title">Números Selecionados</h4>
->>>>>>> 4f87911c82e2f645b6cacac502b1f4c558b487b6
             <p>99, 99, 99, 99</p>
         </div>
     </section>
