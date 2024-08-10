b56ea208ed6b4106ae810769b8c41b88f8d9a154
Luiz R. <luizroberto44630@gmail.com>
2024-08-10 14:55:23 -0300

    Second Commit

---
+++ global.php
@@ -0,0 +1,9 @@
+<?php
+
+// Setup
+define('AC_TERREIRO_VERSION', '1.0.0.'.rand(111111,999999));
+define('AC_TERREIRO_NAME', 'Leje Costumizações');
+define('AC_TERREIRO_PATH', plugins_url('/', __FILE__));
+define('AC_TERREIRO_PATH_URL', __file__);
+define('AC_TERREIRO_PATH_SERVER', plugin_dir_path( __FILE__ ));
+define('AC_TERREIRO_SLUG', 'ac_terreiro/ac_terreiro.php');
---
+++ includes/templates/concursos/details.php
@@ -0,0 +1,62 @@
+<main class="ac-content_widget">
+    <section class="ac-banner">
+        <div class="ac-banner_img">
+            
+        </div>
+        <div class="ac-banner_desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur incidunt voluptatum dolores a quidem tempora. Nihil, porro itaque pariatur quidem alias nesciunt a! Nemo minima iusto eligendi reprehenderit facere nobis?</p>
+        </div>
+    </section>
+    <section class="ac-numbers_select">
+        <div class="ac-select">
+            <h4 class="h4-title ac-select-title">Selecione os Números: </h4>
+            <div class="ac-btns_selection">
+                <button class="btn_select_manual">Selecionar Manualmente</button>
+                <button class="btn_select_auto">Selecionar Automaticamente</button>
+            </div>
+        </div>
+        <div class="ac-grid_numbers">
+            <button class="ac-grid_item chosen">0001</button>
+            <button class="ac-grid_item chosen">0002</button>
+            <button class="ac-grid_item">0003</button>
+            <button class="ac-grid_item">0004</button>
+            <button class="ac-grid_item selected">0005</button>
+            <button class="ac-grid_item selected">0006</button>
+            <button class="ac-grid_item selected">0007</button>
+            <button class="ac-grid_item">0008</button>
+            <button class="ac-grid_item">0009</button>
+            <button class="ac-grid_item chosen">0010</button>
+            <button class="ac-grid_item">0018</button>
+            <button class="ac-grid_item">0029</button>
+            <button class="ac-grid_item chosen">10</button>
+            <button class="ac-grid_item">0078</button>
+            <button class="ac-grid_item">0099</button>
+        </div>
+        <div class="ac-show_numbers">
+            <h4 class="h4-title ">Números Selecionados</h4>
+            <p>99, 99, 99, 99</p>
+        </div>
+    </section>
+    <section class="ac-form_info">
+        <form action="#" method="post">
+            <div class="ac-input_name">
+                <label for="name">Seu Nome:</label>
+                <input type="text" name="name">
+            </div>
+            <div class="ac-input_wpp">
+                <label for="whats">Seu Whatsapp:</label>
+                <input type="text" name="whats">
+            </div>
+        </form>
+    </section>
+    <section class="ac-payment">
+        <div class="ac-container_pay">
+            <div class="ac-total">
+                <p>Total:<br><span>R$ 207,90</span></p>
+            </div>
+            <div class="ac-btn_payment">
+                <button>Ir para o pagamento</button>
+            </div>
+        </div>
+    </section>
+</main>
