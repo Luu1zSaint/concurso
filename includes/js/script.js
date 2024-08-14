569d6ed0987545893a4057030b861dd8ecf3a7ca
Alisson Custodio <forevergamer1@hotmail.com>
2024-08-14 16:30:58 -0300

    Btn auto att

--- includes/js/script.js
+++ includes/js/script.js
@@ -17,6 +17,38 @@ jQuery(document).ready(function($){
     return n_text;
   }
 
+  function in_array(num) {
+    var index = num_choices.indexOf(num);
+    if(index !== -1){
+      num_choices.splice(index, 1);
+      return true;
+    } else {
+      num_choices.push(num);
+      return false;
+    }
+  }
+
+  function att_grid() {
+    let att_grid = fun_allow_nums();
+    let length_grid = att_grid.array_item.length;
+    if( length_grid < 10){
+      $('.ac-allow_nums').text("Números disponíveis: "+length_grid);
+      $(".ac-btn_select_auto").addClass('ac-disabled');
+    }
+    else if (length_grid == 1) {
+      $('.ac-allow_nums').text("Números disponíveis: "+length_grid);
+      $(".ac-btn_select_auto").addClass('ac-disabled');
+    }
+    else if (length_grid > 10) {
+      $('.ac-allow_nums').text("Números disponíveis: "+length_grid);
+      $(".ac-btn_select_auto").removeClass('ac-disabled');
+    }
+    else{
+      $(".ac-btn_select_auto").addClass('ac-disabled');
+      $('.ac-allow_nums').text("Nenhum número disponível");
+    }
+  }
+
   function fun_allow_nums() {
     let grid_nums = $(".ac-grid_numbers button:not(.ac-item_block, .ac-active)");
     let allow_nums = [];
@@ -29,28 +61,25 @@ jQuery(document).ready(function($){
     };
   }
   
-
   function number_gem(n, array_nums, array_item) {
-    
-    console.log(array_nums);
     let i = 0;
     while (i < n) {
       let random_num = Math.floor(Math.random()*100);
-      if($.inArray(String(random_num), array_nums) !== -1){
-
-        var index = num_choices.indexOf(random_num);
-
-        if(index == 1){
-          continue;
-        } else {
-          num_choices.push(random_num);
-        }
+      let array_nums = fun_allow_nums();
+      if($.inArray(String(random_num), array_nums.array_nums) !== -1){
         
+        let check_num = in_array(random_num);
+        if(check_num){ 
+          continue;
+        }
+
         array_item.each(function() {
           if ($(this).text() == String(random_num)) {
-            $(this).addClass("ac-active")}});
+            $(this).addClass("ac-active")
+          }
+        });
         
-        i++;
+        i++;  
       }
     }
     
@@ -62,13 +91,13 @@ jQuery(document).ready(function($){
     let text_choice = num_choices.length > 1 ? "Números selecionados:" : "Número selecionado:";
     let val_total = num_choices.length * parseFloat(unit_val);
 
-    fun_allow_nums();
     return [n_text, text_choice, val_total];
   }
   $("#ac-btn_confirmar").on('click', function() {
-    let qtd_nums = parseInt($("#ac-qtd_nums").val());
-
     let allow_nums = fun_allow_nums()
+    check_length = allow_nums.array_nums.length;
+
+    let qtd_nums = check_length > parseInt($("#ac-qtd_nums").val()) ? parseInt($("#ac-qtd_nums").val()) : check_length;
 
     let result = number_gem(qtd_nums, allow_nums.array_nums, allow_nums.array_item);
 
@@ -78,6 +107,8 @@ jQuery(document).ready(function($){
       $(".ac-show_numbers, .ac-payment, #ac-qtd_num_selects").hide();
     }
 
+    att_grid();
+
     $('#ac-resultNums').text(result[0]);
     $('#ac-nums_selected, #ac-qtd_num_select').text(result[1]);
     $('#ac-qtd_num_select').text(num_choices.length+" "+result[1]);
@@ -85,23 +116,18 @@ jQuery(document).ready(function($){
   });
 
   $(".ac-btn_modal").on('click', function() {
-    var value_toAdd = parseInt($(this).attr('value'));
+    var value_add = parseInt($(this).attr('value'));
     var current_value = parseInt($("#ac-qtd_nums").val()) || 0;
-    var new_value = current_value + value_toAdd;
+    var new_value = current_value + value_add;
     $("#ac-qtd_nums").val(new_value);
-  
+    
   });
 
   $(".ac-grid_item").on('click', function(){
     $(this).toggleClass('ac-active');
-    var this_n = $(this).text();
-    var index = num_choices.indexOf(this_n);
+    var this_n = parseInt($(this).text());
 
-    if(index !== -1){
-      num_choices.splice(index, 1);
-    } else {
-      num_choices.push(this_n);
-    }
+    in_array(this_n);
 
     num_choices.sort(function(a, b) {
         return a - b;
@@ -115,19 +141,23 @@ jQuery(document).ready(function($){
       $(".ac-show_numbers, .ac-payment, #ac-qtd_num_selects").hide();
     }
 
-
     let text_choice = num_choices.length > 1 ? "Números selecionados:" : "Número selecionado:";
     let val_total = num_choices.length * parseFloat(unit_val);
 
-    
+    att_grid();
+
     $('#ac-resultNums').text(n_text);
     $('#ac-nums_selected, #ac-qtd_num_select').text(text_choice);
     $('#ac-qtd_num_select').text(num_choices.length+" "+text_choice);
     $('.ac-valor_total').text(val_total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+    console.log(num_choices);
   });
 
   $(document).on('focus', '.ac-celular', function(){
     $(this).mask('(00) 00000-0000');
   });
+
+  att_grid();
+  
 
 });
--- includes/templates/concursos/details.php
+++ includes/templates/concursos/details.php
@@ -6,7 +6,26 @@
             <img class="ac-img_desktop" src="<?= AC_TERREIRO_PATH ?>/includes/img/img-desktop2.png" alt="img">
         </div>
         <div class="ac-banner_desc">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur incidunt voluptatum dolores a quidem tempora. Nihil, porro itaque pariatur quidem alias nesciunt a! Nemo minima iusto eligendi reprehenderit facere nobis?</p>
+        <p class="ac-title">Sobre o concurso de sorte:</p>
+        <pre>
+🎉Grande Concurso de Sorte em Prol do Terreiro de Candomblé "Ilê Axé Xangô e Ogum"🎉
+
+É com imensa alegria que anunciamos o nosso Concurso de Sorte em benefício do Terreiro de Candomblé "Ilê Axé Xangô e Ogum"! Este é um momento especial para fortalecer nossa comunidade e apoiar as tradições que nos conectam com nossas raízes.
+
+Participe e concorra a prêmios incríveis que serão sorteados no dia 28 de agosto de 2024. Cada número da sorte custa apenas 10 reais e toda a arrecadação será revertida para apoiar as atividades e o desenvolvimento do nosso terreiro.
+
+💫Prêmios Imperdíveis
+Ao adquirir seu número, além de concorrer, você estará contribuindo diretamente para a preservação e continuidade de nossa herança cultural e espiritual. Junte-se a nós nesta corrente de solidariedade e fé!
+
+🔮Como Participar?
+Adquira quantos números desejar e aumente suas chances de ganhar! Cada número custa R$10,00 e estará disponível até o dia do sorteio. Não perca esta oportunidade de fazer parte de algo maior!
+
+🌟 Sorteio: 28/08/2024
+🌟 Valor do Número: R$150,00
+
+Venha com a gente, traga sua energia positiva e participe deste grande movimento em prol do nosso querido Ilê Axé! Axé para todos! ✨
+
+Boa sorte e que Xangô e Ogum abençoem a todos!</pre>
         </div>
     </section>
     <section class="ac-numbers_select">
@@ -14,7 +33,7 @@
             <h4 class="h4-title ac-select-title">Selecione seus números</h4>
             <div class="ac-btns_selection">
                 <button type="button" class="ac-btn_select ac-active">Selecionar Manualmente</button>
-                <button type="button" class="ac-btn_select" onclick="window.dialog.showModal();">Selecionar Automaticamente</button>
+                <button type="button" class="ac-btn_select ac-btn_select_auto" onclick="window.dialog.showModal();">Selecionar Automaticamente</button>
             </div>
                 <dialog id="dialog">
                     <h2>Quantidade de números aleatórios</h2>
@@ -28,9 +47,12 @@
                     <button type="button" class="ac-btn_modal ac-x" onclick="window.dialog.close();" aria-label="close">Fechar</button>
                 </dialog>
         </div>
+        <div class="ac-allow_nums">
+            <p>Númeor disponíveis: </p>
+        </div>
         <div class="ac-grid_numbers">
             <?php for ($i=1; $i < 101; $i++) { ?>
-                <button type="button" class="ac-grid_item <?= $i > 50 ? 'ac-item_block' : '' ?>"><?=$i?></button>
+                <button type="button" class="ac-grid_item"><?=$i?></button>
             <?php } ?>
         </div> 
         <div class="ac-show_numbers">
